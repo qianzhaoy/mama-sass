de2b8cdf6263ec7d3e9e42bd0309d8840ef5b438
钱招严 <qzy@mamaqunaer.com>
2017-10-18 14:46:08 +0800

    增加样式
    增加表单, 按钮样式

--- src/main.js
+++ src/main.js
@@ -3,3 +3,13 @@ require("./js/temp/");
 require("./js/public");
 
 var $ = require("jquery");
+
+//$(function () {
+//	var scripts = document.createElement("script");
+//	scripts.type = "text/x-jquery-tmpl";
+//	scripts.text = "<h1>标题</h1>";
+//	scripts.id = "components"
+//	document.body.appendChild(scripts);
+//	
+//	 $('#components').tmpl().appendTo(document.body);
+//})
